08b15d7536c91a933c955ed4e4b1b20222b58b92
Sai Manideep Nagulapati <122035805+sai-manideep-exe@users.noreply.github.com>
2025-11-25 15:31:33 +0530

    just going back

--- app.py
+++ app.py
@@ -2,9 +2,10 @@ import os
 import streamlit as st
 import pickle
 import time
-from langchain_openai import OpenAI, OpenAIEmbeddings
+from langchain import OpenAI
 from langchain.chains import RetrievalQAWithSourcesChain
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings
 from langchain.document_loaders import UnstructuredURLLoader
 from langchain.vectorstores import FAISS
 from dotenv import load_dotenv
