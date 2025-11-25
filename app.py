6bd38567c53c6e88dcb5c35a354e23fcda87b401
Sai Manideep Nagulapati <122035805+sai-manideep-exe@users.noreply.github.com>
2025-11-25 15:05:08 +0530

    New Langchain (OpenAI)

--- app.py
+++ app.py
@@ -2,11 +2,10 @@ import os
 import streamlit as st
 import pickle
 import time
-from langchain import OpenAI
+from langchain_openai import OpenAI, OpenAIEmbeddings
 from langchain.chains import RetrievalQAWithSourcesChain
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.document_loaders import UnstructuredURLLoader
-from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
 from dotenv import load_dotenv
 
